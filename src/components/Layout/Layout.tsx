d5823ee59c4c80b21a5b0c496f1bddcb11e78129
Kevin1719 <kevin21.aps2b@gmail.com>
2023-02-07 13:01:06 +0300

    modification du background

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -18,7 +18,7 @@ const Layout: React.FC<LayoutProps> = ( {children} ) => {
         <div className="flex flex-col justify-center text-center items-center h-3/4">
             <h2 className="text-white text-2xl font-medium ">Fashion Tips</h2>
             <h1 className="md:text-5xl text-3xl text-white font-semibold py-5">
-            Items every woman should have
+            Items every man should have
             </h1>
             <div className="text-xl">
                 <Button />
